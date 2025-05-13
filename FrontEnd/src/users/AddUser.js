e232c9a9c6e4b93d5ce828f65c2e284582618b01
MuhammedAshraf <muhammedalmugera21@gmail.com>
2025-05-13 11:51:00 +0300

    Add frontend code under FrontEnd/

---
+++ FrontEnd/src/users/AddUser.js
@@ -0,0 +1,111 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+
+export default function AddUser() {
+
+    let navigate = useNavigate()
+
+    const [user, setUser] = useState({
+        name: "",
+        username: "",
+        email: ""
+    })
+
+    const { name, username, email } = user
+
+    const onInputChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post("http://localhost:8080/user", user)
+        navigate("/")
+    };
+
+    return (
+
+        <div className="container">
+
+            <div className="row">
+
+                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+                    <h2 className="text-center m-4">Register User</h2>
+
+                    <form onSubmit={(e) => onSubmit(e)}>
+
+                        {/* Name */}
+                        <div className="mb-3">
+                            <label htmlFor="name" className="form-label">
+                                Name
+                            </label>
+                            <div id="nameHelp" className="form-text">
+                                Enter the full name of the user.
+                            </div>
+                            <input
+                                type={"text"}
+                                className="form-control"
+                                placeholder="Enter The Name"
+                                name="name"
+                                value={name}
+                                onChange={(e) => onInputChange(e)}
+
+                            />
+                        </div>
+
+                        {/* Username */}
+                        <div className="mb-3">
+                            <label htmlFor="Username" className="form-label">
+                                Username
+                            </label>
+                            <div id="nameHelp" className="form-text">
+                                Enter Correct Username of the user.
+                            </div>
+                            <input
+                                type={"text"}
+                                className="form-control"
+                                placeholder="Enter The Username"
+                                name="username"
+                                value={username}
+                                onChange={(e) => onInputChange(e)}
+                            />
+                        </div>
+
+                        {/* E-mil */}
+                        <div className="mb-3">
+                            <label htmlFor="E-mail" className="form-label">
+                                E-mail
+                            </label>
+                            <div id="nameHelp" className="form-text">
+                                Enter Correct E-mail of the user.
+                            </div>
+                            <input
+                                type={"text"}
+                                className="form-control"
+                                placeholder="Enter The E-mil"
+                                name="email"
+                                value={email}
+                                onChange={(e) => onInputChange(e)}
+                            />
+                        </div>
+
+                        {/* Submit Button*/}
+
+                        <button type="submit" className="btn btn-outline-primary me-2">
+                            Submit
+                        </button>
+
+                        {/* <button type="submit"  className="btn me-2"  style={{ background: "linear-gradient(to right, #0d6efd, #dc3545)", color: "#fff", border: "none", }} > Submit </button> */}
+
+                        {/*Back / Cancel Button*/}
+
+                        <Link className="btn btn-outline-danger mx-2" to="/" >
+                            Cancel
+                        </Link>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
